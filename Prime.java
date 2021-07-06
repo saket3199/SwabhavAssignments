e40905be93c3be86ebc146526d82ed4efde4fbc3
SAket MIshra <saketmishra247@gmail.com>
2021-07-06 20:19:03 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -5,21 +5,22 @@ public class Prime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=2;i<100;i++){
-            int count=1;
-            for(int j=2;j<i;j++){
-                if(i%j ==0){
-                    count=0;
-                    break;
-                }
 
-            }if(count==1){
-                System.out.println(i);
+		for (int i = 2; i < 100; i++) {
+			boolean flag = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					flag = false;
+					break;
+				}
 
-            }
-	
-	}
+			}
+			if (flag) {
+				System.out.println(i);
 
+			}
+
+		}
 	}
 
 }
