bc1939ad3d19881ba29376184e0c706d9cedd50f
SAket MIshra <saketmishra247@gmail.com>
2021-07-06 20:18:10 +0530

    Update ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -5,19 +5,20 @@ public class ArrayOperations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int myArray[]={10,56,78,100,34,57};
-    int temp;
-    for(int i=0;i<myArray.length;i++){
-        for(int j=i+1;j<myArray.length;j++){
-             if(myArray[i] < myArray[j]) {    
-                   temp = myArray[i];    
-                   myArray[i] = myArray[j];    
-                   myArray[j] = temp;    
-             }
-        }
-    }
-    System.out.println(myArray[0]);
-    System.out.println(myArray[1]);
+
+		int myArray[] = { 10, 56, 78, 100, 34, 57 };
+		int length = myArray.length;
+		for (int j = 0; j < length - 1; j++) {
+			if (myArray[j] < myArray[j + 1]) {
+
+				int temp = myArray[j];
+				myArray[j] = myArray[j + 1];
+				myArray[j + 1] = temp;
+				j = -1;
+			}
+		}
+		System.out.println(myArray[0]);
+		System.out.println(myArray[1]);
 	}
 
 }
