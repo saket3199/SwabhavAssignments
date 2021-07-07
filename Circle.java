0797889dac642ae651dfe16ca2f732f2df8add85
SAket MIshra <saketmishra247@gmail.com>
2021-07-07 19:09:48 +0530

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,30 +1,12 @@
-//Write a program to calculate area and perimeter of circle
-package com.techlab.assignment;
-
-import java.util.Scanner;
+package com.techlab.circle;
 
 public class Circle {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Enter 1 for Area of the Circle \n,Enter 2 for Perimeter");
-		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		switch (a) {
-		case 1:
-			System.out.println("Enter the radius : ");
-			int r = scanner.nextInt();
-			System.out.println(3.142 * r * r);
-			break;
-		case 2:
-			System.out.println("Enter the radius : ");
-			int r1 = scanner.nextInt();
-			System.out.println(2 * 3.142 * r1);
-			break;
-		default:
-			System.out.println("Enter a Valid selection");
-		}
-
+	public float radius;
+	public String color;
+	
+	public void display() {
+		System.out.println("Circle Radius is "+radius+" and color is "+color);
 	}
 
 }
