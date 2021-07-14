140e8b999c30f31c8bb3abe072c73729a0a47cd0
SAket MIshra <saketmishra247@gmail.com>
2021-07-14 14:45:04 +0530

    Update EngineeringData.java

--- Day8/engineering/test/EngineeringData.java
+++ Day8/engineering/test/EngineeringData.java
@@ -9,8 +9,12 @@ public class EngineeringData {
 		// TODO Auto-generated method stub
 		Student s=new Student(1,"Kandivali", "31/07/1999","Computer Science");
 		Professor p=new Professor(1,"Mumbai","31/07/1990",50000);
+		p.setConveyance(0);
+		p.setDearness_Allowance(0);
+		p.setHRA_Allowance(0);
+		p.setMedical_allowance(0);
 		System.out.println("Id of Student = "+s.getId()+" Address = "+s.getAddress()+" DOB = "+s.getDOB()+" Branch = "+s.getBranch());
-		System.out.println("Id of Professor = "+p.getId()+" Address = "+p.getAddress()+" DOB = "+p.getDOB()+" Salary = "+p.getSalary());
+		System.out.println("Id of Professor = "+p.getId()+" Address = "+p.getAddress()+" DOB = "+p.getDOB()+" Basic Salary = "+p.getSalary()+" Total Salary = "+p.totalSalary());
 	}
 
 }
